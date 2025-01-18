736bc618a8693fd639e097ecd3cec7bafb08eede
proman3419 <proman7268@gmail.com>
2025-01-18 17:43:07 +0100

    Replace id with key

--- main.py
+++ main.py
@@ -3,52 +3,43 @@ from copy import deepcopy
 
 
 app = FastAPI()
-messages = []
+messages = {}
 
 
 class Message:
-    def __init__(self, id_, content):
-        self.id_ = id_
+    def __init__(self, key, content):
+        self.key = key
         self.content = content
 
     def to_dict(self):
-        return {"id": self.id_, "content": self.content}
-
-
-def find_message_by_id(id_):
-    id_ = int(id_)
-    for i, m in enumerate(messages):
-        if m.id_ == id_:
-            return i, m
-    return -1, None
+        return {"key": self.key, "content": self.content}
 
 
 @app.post("/message")
-def add_message(content):
-    id_ = 0 if not messages else messages[-1].id_ + 1
-    message = Message(id_, content)
-    messages.append(message)
+def add_message(key, content):
+    message = Message(key, content)
+    messages[key] = message
     return message.to_dict()
 
 
 @app.get("/message")
-def get_message(id_):
-    _, message = find_message_by_id(id_)
-    if not message:
+def get_message(key):
+    if key not in messages:
         raise HTTPException(status_code=404)
+    message = messages[key]
     return message.to_dict()
 
 
 @app.get("/messages")
 def get_messages():
-    return {"messages": [message.to_dict() for message in messages]}
+    return {"messages": [message.to_dict() for message in messages.values()]}
 
 
 @app.delete("/message")
-def delete_message(id_):
-    i, message = find_message_by_id(id_)
-    if not message:
+def delete_message(key):
+    if key not in messages:
         raise HTTPException(status_code=404)
+    message = messages[key]
     message_dc = deepcopy(message)
-    del messages[i]
+    del messages[key]
     return message_dc.to_dict()
